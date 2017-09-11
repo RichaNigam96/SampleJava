2c22aa15b35e182a732d01300222ca1cb12212ca
RichaNigam96 <30289612+RichaNigam96@users.noreply.github.com>
2017-09-11 10:56:51 +0530

    Deleted all

--- JavaSample/tools/DoNothingModifier.java
+++
@@ -1,7 +0,0 @@
-package com.amazon.dw.grasshopper.tools;
-
-import com.amazon.dw.grasshopper.tools.ghqmodifiers.AbstractGHQModifier;
-
-public class DoNothingModifier extends AbstractGHQModifier {
-    public DoNothingModifier(){}
-}
